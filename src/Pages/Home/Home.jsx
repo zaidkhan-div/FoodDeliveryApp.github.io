2cea41db3ddb9e0731ebe320cbb0b00ca92ba9e1
Zaidkhan <zaidscestudent@gmail.com>
2024-08-28 15:33:01 +0500

    almost Completed and responsives too

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../Components/Header/Header'
 import Exploremenu from '../../Components/ExplorMenu/Exploremenu'
 import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
+import Appdownload from '../../Components/AppDownload/Appdownload'
 const Home = () => {
   const [category, setCategory] = useState('All')
 
@@ -10,7 +11,8 @@ const Home = () => {
     <div>
       <Header />
       <Exploremenu category={category} setCategory={setCategory} />
-      <FoodDisplay category={category}/>
+      <FoodDisplay category={category} />
+      <Appdownload />
     </div>
   )
 }
